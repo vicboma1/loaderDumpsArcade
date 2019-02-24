7161c3de8ecca413bb17860a139888de7cd0d86a
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-02-25 00:31:42 +0100

    Add files via upload

---
+++ assets/Common/Ext/StringExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    //Método de extensión...
+    public static class StringExt
+    {
+        public static String toHex(this int i) =>
+            toHex(new IntPtr(i)); 
+
+        public static String toHex(this IntPtr ptr) =>
+            "0x" + ptr.ToString("X8"); 
+    }
+
+
+    
+}
