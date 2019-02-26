eb72c7c2fc35e179ff8cf129babb3ba706ec3ffd
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-02-26 20:20:23 +0100

    Update IEDAByte.cs

--- assets/FormInputDevice/Inputs/Byte/IEDAByte.cs
+++ assets/FormInputDevice/Inputs/Byte/IEDAByte.cs
@@ -7,8 +7,20 @@ namespace FormInputDevice.Inputs.Byte
 {
     public interface IEDAByte
     {
-        IDictionary<int, IDictionary<String, object>> map { get; set; }
-        byte[] _byte { get; set; }
-        int this[int id] { get; set; }
+        IEDAByte SetFileBinary(byte[] data);
+        IEDAByte SetFileJson(IDictionary<int, IDictionary<object, object>> data);
+
+        int GetByte(int it);
+        String GetHex(int it);
+        int GetDec(int it);
+        String GetKey(int it);
+        
+        IEDAByte SetByte(int pos, int it);
+        IEDAByte SetHex(int pos, String it);
+        IEDAByte SetDec(int pos, int it);
+        IEDAByte SetKey(int pos, String it);
+
+        byte[] GetData();
+        IDictionary<int, IDictionary<object, object>> GetDebug();
     }
 }
