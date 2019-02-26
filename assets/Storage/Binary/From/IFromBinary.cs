c2faf47dc9239cb76788c465d89276b72980952b
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-02-26 20:22:42 +0100

    Add files via upload

---
+++ assets/Storage/Binary/From/IFromBinary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    public interface IFromBinary
+    {
+        Task<byte[]> ReaderAsync(String file);
+        Task<byte[]> ReaderAsyncMonitor();
+    }
+}
---
+++ assets/Storage/Binary/To/IToBinary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    public interface IToBinary
+    {
+        Task<bool> WriterAsync(byte[] data, String file);
+        Task<bool> WriterAsyncMonitor(byte[] data);
+    }
+}
