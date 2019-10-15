c07d294f952edc164b0117567b7a80ae4a31bfa6
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-10-15 22:03:29 +0200

    Update pTaskBar.h
    refactors

--- assets/Win32/Utils/TaskBar/pTaskBar.h
+++ assets/Win32/Utils/TaskBar/pTaskBar.h
@@ -1,19 +1,30 @@
-#ifndef _SHOW_TASKBAR_H_
-#define _SHOW_TASKBAR_H_
+#ifndef _TASK_BAR_DIR_H_
+#define _TASK_BAR_DIR_H_
 
 #include "stdafx.h"
+#include <cstdint>
 
-class PShowTaskBar {
+class TaskBar {
 
 private :
+
+	#if INTPTR_MAX == INT32_MAX
+		#define WIN_TASKBAR "Shell_TrayWnd"
+	#elif INTPTR_MAX == INT64_MAX
+		#define WIN_TASKBAR "Button"
+	#else
+		#error "Environment not 32 or 64-bit."
+	#endif
+
+	const char* winTaskBar = WIN_TASKBAR;
 	bool _valueHidden;
 
 public:
-	PShowTaskBar(bool);
-	~PShowTaskBar();
+	TaskBar(bool);
+	~TaskBar();
 
-	void __stdcall show(void);
-	bool __stdcall isHidden(void);
-	void __stdcall hidden(void);
+	void _stdcall show(void);
+	bool _stdcall isHidden(void);
+	void _stdcall hidden(void);
 };
 #endif
