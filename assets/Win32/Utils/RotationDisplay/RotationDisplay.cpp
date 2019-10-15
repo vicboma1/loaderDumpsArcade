c3b01c2305ac1f1108f969a053c035b813f89e39
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-10-15 22:07:32 +0200

    Update RotationDisplay.cpp

--- assets/Win32/Utils/RotationDisplay/RotationDisplay.cpp
+++ assets/Win32/Utils/RotationDisplay/RotationDisplay.cpp
@@ -12,7 +12,7 @@ RotationDisplay::~RotationDisplay() {
 		free(this);
 }
 
-bool RotationDisplay::start(int angle) {
+bool _stdcall RotationDisplay::start(int angle) {
 	LOGGER();
 
 	if (angle < DMDO_DEFAULT || angle > DMDO_270)
@@ -28,34 +28,34 @@ bool RotationDisplay::start(int angle) {
 	if (DMDO_DEFAULT == angle)
 		return true;
 
-	DWORD temp = dm.dmPelsHeight;
+	auto temp = dm.dmPelsHeight;
 	dm.dmPelsHeight = dm.dmPelsWidth;
 	dm.dmPelsWidth = temp;
 
 	dm.dmDisplayOrientation = angle;
 
-	LONG res = ChangeDisplaySettings(&dm, 0);
+	auto res = ChangeDisplaySettings(&dm, 0);
 	//Sleep(3000);
 	return  res == DISP_CHANGE_SUCCESSFUL;
 }
 
-bool RotationDisplay::_DEFAULT() {
+bool _stdcall RotationDisplay::_DEFAULT() {
 	return start(DMDO_DEFAULT);
 }
 
-bool RotationDisplay::_90() {
+bool _stdcall RotationDisplay::_90() {
 	return start(DMDO_90);
 }
 
-bool RotationDisplay::_180() {
+bool _stdcall RotationDisplay::_180() {
 	return start(DMDO_180);
 }
 
-bool RotationDisplay::_270() {
+bool _stdcall RotationDisplay::_270() {
 	return start(DMDO_270);
 }
 
-bool RotationDisplay::stop(int _angle) {
+bool _stdcall RotationDisplay::stop(int _angle) {
 	LOGGER();
 
 	if (_angle <= -1 || _angle >= 4 || _angle == 0)
@@ -71,7 +71,7 @@ bool RotationDisplay::stop(int _angle) {
 	if (!EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dm))
 		return false;
 
-	DWORD temp = dm.dmPelsHeight;
+	auto temp = dm.dmPelsHeight;
 	dm.dmPelsHeight = dm.dmPelsWidth;
 	dm.dmPelsWidth = temp;
 	dm.dmDisplayOrientation = DMDO_DEFAULT;
@@ -79,6 +79,6 @@ bool RotationDisplay::stop(int _angle) {
 	return true;
 }
 
-bool RotationDisplay::stop() {
+bool _stdcall RotationDisplay::stop() {
 	return stop(DMDO_90);
 }
