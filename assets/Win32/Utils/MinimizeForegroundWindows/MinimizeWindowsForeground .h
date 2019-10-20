223cbf1564d0dc90b28570c735d65fd7a187e5c3
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-10-20 19:48:37 +0200

    Add files via upload

---
+++ assets/Win32/Utils/MinimizeForegroundWindows/MinimizeWindowsForeground .h
@@ -0,0 +1,14 @@
+#ifndef _MINIMIZE_WINDOWS_FOREGROUND_
+#define _MINIMIZE_WINDOWS_FOREGROUND_
+
+class MinimizeWindowsForeground 
+{
+	private :
+		
+	public:
+		 MinimizeWindowsForeground ();
+		~MinimizeWindowsForeground ();
+		bool execute();
+};
+
+#endif
