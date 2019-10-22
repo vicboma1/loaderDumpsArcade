18fd5bb92510638c5c701884ccea3c92b6724428
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-10-22 23:26:23 +0200

    Update DestroyProcessAsync.cs

--- assets/Daemon/DestroyProcessAsync/DestroyProcessAsync.cs
+++ assets/Daemon/DestroyProcessAsync/DestroyProcessAsync.cs
@@ -10,28 +10,20 @@ using System.Windows.Forms;
 
 namespace DestroProcessAsync
 {
-    class DestroProcessAsync
+    class Program
     {
-        [DllImport("user32.dll")]
-        static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32")]
-        public static extern int RegisterWindowMessage(string message);
-
-        private string PROCESS = "Loader";
+        private string PROCESS_WIN_NAME = "Loader";
         private static readonly Process _p;
 
         static void Main(string[] args)
         {
-			 if (null == args || args.Length > 2 )
+		if (null == args || args.Length > 2 )
                 return;
 			
-			if( 1 == args.Length)
-				PROCESS = args[0];
+		if( 1 == args.Length)
+			PROCESS_WIN_NAME = args[0];
 			
             Task.WaitAny(MainAsync());
-
-			SetForegroundWindow(_p.MainWindowHandle);
 			
             if (Application.AllowQuit)
                 Application.ExitThread();
@@ -42,11 +34,10 @@ namespace DestroProcessAsync
 
         public static async Task MainAsync()
         {          
-		    bool m1 = false;
-            Process[] ps;
+		bool m1 = false;
+		Process[] ps;
          
-   		    await Task.Run(async () =>
-            {
+   		await Task.Run(async () => {
                 while (!m1)
                 {
                     ps = Process.GetProcesses();
@@ -62,10 +53,10 @@ namespace DestroProcessAsync
                     await Task.Delay(50);
                 }
 
-                    if (_p != null){                  
-						_p.CloseMainWindow();
-                		_p.Close();
-					}
+                    if (_p != null){
+			    _p.CloseMainWindow();
+			    _p.Close();
+		    }
             });
 
         }
