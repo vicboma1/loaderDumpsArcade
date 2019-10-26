d75b8994bd23ccb12f77bab361c03d5a28dd5f3d
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-10-27 00:39:53 +0200

    Add files via upload

---
+++ assets/Win32/Utils/HasLetterDrive/HasLetterDrive.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PartitionDisk
+{
+    class Program {
+		
+        private static String D = @"D:\";
+
+         static int Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                MessageBox.Show(null, "Falta parametros de entrada", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -1;
+            }
+
+            var allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.Name.ToLower().Equals(D.ToLower()))
+                    return 1;
+               
+            }
+			
+			return 0;
+
+        }
+          
+    }
+}
