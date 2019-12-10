89e4d90aaea02c5ac53eac2522f22e01e4536538
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-12-10 22:07:24 +0100

    Update Game.cpp

--- assets/Platform/Enums/Games/Game.cpp
+++ assets/Platform/Enums/Games/Game.cpp
@@ -1,13 +1,14 @@
 #include "stdafx.h"
 
+//refactor https://github.com/vicboma1/loaderDumpsArcade/issues/45
 Game::Game() {
 	LOG_API();
 }
 
 Game::~Game() {
 	LOG_API();
-	if (this)
-		delete this;
+	//if (this)
+	//	delete this;
 }
 
 const char* Game::toString(CONFIG_GAME_ENUM i) {
