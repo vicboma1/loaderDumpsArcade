31e4ee93260dd4b0d35e6ed3d3bf2e624ec9b894
Victor Bolinches <vicboma1@users.noreply.github.com>
2020-01-17 00:24:28 +0100

    Update iDmacDrv32Hook.h

--- assets/Platform/Hooks/iDmacDrv32/iDmacDrv32Hook.h
+++ assets/Platform/Hooks/iDmacDrv32/iDmacDrv32Hook.h
@@ -1,57 +1,57 @@
 #include <Windows.h>
 
-typedef int(__cdecl  *LPiDmacDrvRegisterWrite)(int deviceId, DWORD CommandCode, int unused, DWORD *DeviceResult);
+typedef int(__stdcall  *LPiDmacDrvRegisterWrite)(int deviceId, DWORD CommandCode, int unused, DWORD *DeviceResult);
 static LPiDmacDrvRegisterWrite __iDmacDrvRegisterWrite = NULL;
-int __cdecl  _H_iDmacDrvRegisterWrite(int deviceId, DWORD CommandCode, int unused, DWORD *DeviceResult);
+int __stdcall  _H_iDmacDrvRegisterWrite(int deviceId, DWORD CommandCode, int unused, DWORD *DeviceResult);
 
-typedef DWORD(__cdecl  *LPiDmacDrvOpen)(int deviceId, LPVOID outBuffer, LPVOID lpSomeFlag);
+typedef DWORD(__stdcall  *LPiDmacDrvOpen)(int deviceId, LPVOID outBuffer, LPVOID lpSomeFlag);
 static LPiDmacDrvOpen __iDmacDrvOpen = NULL;
-DWORD __cdecl _H_iDmacDrvOpen(int deviceId, LPVOID outBuffer, LPVOID lpSomeFlag);
+DWORD __stdcall _H_iDmacDrvOpen(int deviceId, LPVOID outBuffer, LPVOID lpSomeFlag);
 
-typedef int(__cdecl  *LPiDmacDrvRegisterRead)(int deviceId, DWORD CommandCode, LPVOID OutBuffer, LPVOID DeviceResult);
+typedef int(__stdcall  *LPiDmacDrvRegisterRead)(int deviceId, DWORD CommandCode, LPVOID OutBuffer, LPVOID DeviceResult);
 static LPiDmacDrvRegisterRead __iDmacDrvRegisterRead = NULL;
-int __cdecl _H_iDmacDrvRegisterRead(int deviceId, DWORD CommandCode, LPVOID OutBuffer, LPVOID DeviceResult);
+int __stdcall _H_iDmacDrvRegisterRead(int deviceId, DWORD CommandCode, LPVOID OutBuffer, LPVOID DeviceResult);
 
-typedef DWORD(__cdecl  *LPiDmacDrvClose)(int deviceId, LPVOID lpWriteAccess);
+typedef DWORD(__stdcall  *LPiDmacDrvClose)(int deviceId, LPVOID lpWriteAccess);
 static LPiDmacDrvClose __iDmacDrvClose = NULL;
-DWORD __cdecl _H_iDmacDrvClose(int deviceId, LPVOID lpWriteAccess);
+DWORD __stdcall _H_iDmacDrvClose(int deviceId, LPVOID lpWriteAccess);
 
-typedef int(__cdecl *LPiDmacDrvDmaRead)(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
+typedef int(__stdcall *LPiDmacDrvDmaRead)(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
 static LPiDmacDrvDmaRead __iDmacDrvDmaRead = NULL;
-int __cdecl _H_iDmacDrvDmaRead(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
+int __stdcall _H_iDmacDrvDmaRead(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
 
-typedef int(__cdecl *LPiDmacDrvDmaWrite)(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
+typedef int(__stdcall *LPiDmacDrvDmaWrite)(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
 static LPiDmacDrvDmaWrite __iDmacDrvDmaWrite = NULL;
-int __cdecl _H_iDmacDrvDmaWrite(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
+int __stdcall _H_iDmacDrvDmaWrite(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
 
-typedef int(__cdecl *LPiDmacDrvRegisterBufferRead)(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+typedef int(__stdcall *LPiDmacDrvRegisterBufferRead)(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 static LPiDmacDrvRegisterBufferRead __iDmacDrvRegisterBufferRead = NULL;
-int __cdecl _H_iDmacDrvRegisterBufferRead(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+int __stdcall _H_iDmacDrvRegisterBufferRead(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 
-typedef int(__cdecl *LPiDmacDrvRegisterBufferWrite)(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+typedef int(__stdcall *LPiDmacDrvRegisterBufferWrite)(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 static LPiDmacDrvRegisterBufferWrite __iDmacDrvRegisterBufferWrite = NULL;
-int __cdecl _H_iDmacDrvRegisterBufferWrite(int a1, DWORD BytesReturned, LPVOID *lp, UINT_PTR ucb, LPVOID a5);
+int __stdcall _H_iDmacDrvRegisterBufferWrite(int a1, DWORD BytesReturned, LPVOID *lp, UINT_PTR ucb, LPVOID a5);
 
-typedef int(__cdecl *LPiDmacDrvMemoryBufferWrite)(int a1, int a2, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+typedef int(__stdcall *LPiDmacDrvMemoryBufferWrite)(int a1, int a2, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 static LPiDmacDrvMemoryBufferWrite __iDmacDrvMemoryBufferWrite = NULL;
-int __cdecl _H_iDmacDrvMemoryBufferWrite(int a1, int a2, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+int __stdcall _H_iDmacDrvMemoryBufferWrite(int a1, int a2, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 
-typedef int(__cdecl *LPiDmacDrvMemoryBufferRead)(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+typedef int(__stdcall *LPiDmacDrvMemoryBufferRead)(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 static LPiDmacDrvMemoryBufferRead __iDmacDrvMemoryBufferRead = NULL;
-int __cdecl _H_iDmacDrvMemoryBufferRead(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+int __stdcall _H_iDmacDrvMemoryBufferRead(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
 
-typedef int(__cdecl *LPiDmacDrvMemoryWriteExt)(int a1, int a2, int a3, LPVOID Src, rsize_t DstSize, LPVOID lp);
+typedef int(__stdcall *LPiDmacDrvMemoryWriteExt)(int a1, int a2, int a3, LPVOID Src, rsize_t DstSize, LPVOID lp);
 static LPiDmacDrvMemoryWriteExt __iDmacDrvMemoryWriteExt = NULL;
-int __cdecl _H_iDmacDrvMemoryWriteExt(int a1, int a2, int a3, LPVOID Src, rsize_t DstSize, LPVOID lp);
+int __stdcall _H_iDmacDrvMemoryWriteExt(int a1, int a2, int a3, LPVOID Src, rsize_t DstSize, LPVOID lp);
 
-typedef int(__cdecl *LPiDmacDrvMemoryReadExt)(int a1, DWORD BytesReturned, int a3, LPVOID lp, DWORD nOutBufferSize, LPVOID a6);
+typedef int(__stdcall *LPiDmacDrvMemoryReadExt)(int a1, DWORD BytesReturned, int a3, LPVOID lp, DWORD nOutBufferSize, LPVOID a6);
 static LPiDmacDrvMemoryReadExt __iDmacDrvMemoryReadExt = NULL;
-int __cdecl _H_iDmacDrvMemoryReadExt(int a1, DWORD BytesReturned, int a3, LPVOID lp, DWORD nOutBufferSize, LPVOID a6);
+int __stdcall _H_iDmacDrvMemoryReadExt(int a1, DWORD BytesReturned, int a3, LPVOID lp, DWORD nOutBufferSize, LPVOID a6);
 
-typedef int(__cdecl *LPiDmacDrvMemoryWrite)(int a1, DWORD BytesReturned, int a3, LPVOID lp);
+typedef int(__stdcall *LPiDmacDrvMemoryWrite)(int a1, DWORD BytesReturned, int a3, LPVOID lp);
 static LPiDmacDrvMemoryWrite __iDmacDrvMemoryWrite = NULL;
-int __cdecl _H_iDmacDrvMemoryWrite(int a1, DWORD BytesReturned, int a3, LPVOID lp);
+int __stdcall _H_iDmacDrvMemoryWrite(int a1, DWORD BytesReturned, int a3, LPVOID lp);
 
-typedef int(__cdecl *LPiDmacDrvMemoryRead)(int a1, DWORD BytesReturned, LPVOID lp, LPVOID a4);
+typedef int(__stdcall *LPiDmacDrvMemoryRead)(int a1, DWORD BytesReturned, LPVOID lp, LPVOID a4);
 static LPiDmacDrvMemoryRead __iDmacDrvMemoryRead = NULL;
-int __cdecl _H_iDmacDrvMemoryRead(int a1, DWORD BytesReturned, LPVOID lp, LPVOID a4);
+int __stdcall _H_iDmacDrvMemoryRead(int a1, DWORD BytesReturned, LPVOID lp, LPVOID a4);
