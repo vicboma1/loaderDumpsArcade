33e5018227f6f9f0dc286f7b94e203e80ba6fa10
Victor Bolinches <vicboma1@users.noreply.github.com>
2020-02-09 22:31:08 +0100

    Update glu32Hook.h

--- assets/Platform/Hooks/glu32/glu32Hook.h
+++ assets/Platform/Hooks/glu32/glu32Hook.h
@@ -8,21 +8,21 @@
 	//https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/gluPerspective.xml
 	typedef void(__stdcall *LPgluPerspective)(double fovy, double aspect, double zNear, double zFar);
 	static LPgluPerspective __gluPerspective = NULL;
-	void __stdcall Hook_gluPerspective(double fovy, double aspect, double zNear, double zFar);
+	void __stdcall _H_gluPerspective(double fovy, double aspect, double zNear, double zFar);
 
 	//https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/gluOrtho2D.xml
 	typedef void(__stdcall *LPgluOrtho2D)(double left, double right, double bottom, double top);
 	static LPgluOrtho2D __gluOrtho2D = NULL;
-	void __stdcall Hook_gluOrtho2D(double left, double right, double bottom, double top);
+	void __stdcall _H_gluOrtho2D(double left, double right, double bottom, double top);
 
 	//https://www.opengl.org/resources/libraries/glut/spec3/node11.html
 	typedef void(__stdcall *LPglutInitWindowSize)( int width, int height);
 	static LPglutInitWindowSize __glutInitWindowSize = NULL;
-	void __stdcall Hook_glutInitWindowSize(int width, int height);
+	void __stdcall _H_glutInitWindowSize(int width, int height);
 
 	//https://www.opengl.org/resources/libraries/glut/spec3/node11.html
 	typedef void(__stdcall *LPglutReshapeWindow)(int width, int height);
 	static LPglutReshapeWindow __glutReshapeWindow = NULL;
-	void __stdcall Hook_glutReshapeWindow(int width, int height);
+	void __stdcall _H_glutReshapeWindow(int width, int height);
 
 #endif
