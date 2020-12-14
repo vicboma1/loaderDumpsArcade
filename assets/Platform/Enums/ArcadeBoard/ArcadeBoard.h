ffb7b162e78e0a1dd49b6ad260f26c969d4d64d9
Victor Bolinches <vicboma1@users.noreply.github.com>
2020-12-14 15:24:32 +0100

    Update ArcadeBoard.h

--- assets/Platform/Enums/ArcadeBoard/ArcadeBoard.h
+++ assets/Platform/Enums/ArcadeBoard/ArcadeBoard.h
@@ -5,9 +5,10 @@
 #include "Macros.h"
 
 #define FOREACH_ARCADE_BOARD(ARCADE_BOARD)  	\
-    ARCADE_BOARD(NONE)				\
     ARCADE_BOARD(TAITO)   			\
-    ARCADE_BOARD(NESICA)			
+    ARCADE_BOARD(TAITO_2)   			\
+    ARCADE_BOARD(NESICA)			\
+    ARCADE_BOARD(NONE)
 
 enum CONFIG_ARCADE_BOARD_ENUM {
 	FOREACH_ARCADE_BOARD(GENERATE_ENUM)
@@ -15,28 +16,6 @@ enum CONFIG_ARCADE_BOARD_ENUM {
 
 static const char *CONFIG_ARCADE_BOARD_ENUM_STRING[] = {
 	FOREACH_ARCADE_BOARD(GENERATE_STRING)
-};
-
-class ArcadeBoard
-{
-public:
-
-	ArcadeBoard();
-	~ArcadeBoard();
-	const char* toString(CONFIG_ARCADE_BOARD_ENUM i);
-	CONFIG_ARCADE_BOARD_ENUM toEnum(string i);
-	void set(CONFIG_ARCADE_BOARD_ENUM i);
-	CONFIG_ARCADE_BOARD_ENUM get();
-
-private:
-
-	CONFIG_ARCADE_BOARD_ENUM _enum;
-
-	unordered_map<string, CONFIG_ARCADE_BOARD_ENUM>  mapStringEnum = {
-	   { CONFIG_ARCADE_BOARD_ENUM_STRING[0], NONE},
-	   { CONFIG_ARCADE_BOARD_ENUM_STRING[1], TAITO},
-	   { CONFIG_ARCADE_BOARD_ENUM_STRING[2], NESICA}
-	};
 };
 
 
